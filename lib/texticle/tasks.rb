be46d90c400a13427ed31853a9007970819590ab
Aaron Patterson <aaron.patterson@gmail.com>
2009-04-14 15:55:14 -0700

    fixing rake tasks

--- lib/texticle/tasks.rb
+++ lib/texticle/tasks.rb
@@ -1,23 +1,25 @@
 require 'rake'
 require 'texticle'
 
-namespace :texticle => [:environment] do
-  task :create_indexes do
+namespace :texticle do
+  desc "Create full text indexes"
+  task :create_indexes => [:environment] do
     Dir[File.join(RAILS_ROOT, 'app', 'models', '*.rb')].each do |f|
       klass = File.basename(f, '.rb').classify.constantize
       if klass.respond_to?(:full_text_indexes)
-        klass.full_text_indexes.each do |fti|
+        (klass.full_text_indexes || []).each do |fti|
           fti.create
         end
       end
     end
   end
 
-  task :destroy_indexes do
+  desc "Destroy full text indexes"
+  task :destroy_indexes => [:environment] do
     Dir[File.join(RAILS_ROOT, 'app', 'models', '*.rb')].each do |f|
       klass = File.basename(f, '.rb').classify.constantize
       if klass.respond_to?(:full_text_indexes)
-        klass.full_text_indexes.each do |fti|
+        (klass.full_text_indexes || []).each do |fti|
           fti.destroy
         end
       end
