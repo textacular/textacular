7cb232e972776802979639e1db1d942169546626
Aaron Patterson <aaron.patterson@gmail.com>
2009-04-14 11:44:17 -0700

    weights are set

--- lib/texticle/full_text_index.rb
+++ lib/texticle/full_text_index.rb
@@ -9,9 +9,30 @@ module Texticle
       instance_eval(&block)
     end
 
+    def create
+      @model_class.connection.execute(<<-eosql)
+        CREATE index #{@name}
+        ON #{@model_class.table_name}
+        USING gin((#{to_s}))
+      eosql
+    end
+
+    def to_s
+      vectors = []
+      @index_columns.sort_by { |k,v| k }.each do |weight, columns|
+        if weight == 'none'
+          vectors << "to_tsvector('english', #{columns.join(", ")})"
+        else
+          vectors <<
+        "setweight(to_tsvector('english', #{columns.join(", ")}), '#{weight}')"
+        end
+      end
+      vectors.join(" || ' ' || ")
+    end
+
     def method_missing name, *args
-      weight = args.shift || :none
-      index_columns[weight] = name.to_s
+      weight = args.shift || 'none'
+      (index_columns[weight] ||= []) << name.to_s
     end
   end
 end
--- test/test_full_text_index.rb
+++ test/test_full_text_index.rb
@@ -18,6 +18,8 @@ class TestFullTextIndex < Test::Unit::TestCase
     def execute sql
       @executed << sql
     end
+
+    def table_name; 'fake_model'; end
   end
 
   def test_initialize
@@ -26,7 +28,53 @@ class TestFullTextIndex < Test::Unit::TestCase
       name
       value 'A'
     end
-    assert_equal 'name',  fti.index_columns[:none]
-    assert_equal 'value', fti.index_columns['A']
+    assert_equal 'name',  fti.index_columns['none'].first
+    assert_equal 'value', fti.index_columns['A'].first
+  end
+
+  def test_create
+    fm = FakeModel.new
+    fti = Texticle::FullTextIndex.new('ft_index', fm) do
+      name
+      value 'A'
+    end
+    fti.create
+    assert fm.connected
+    assert_equal 1, fm.executed.length
+  end
+
+  def test_to_s_no_weight
+    fm = FakeModel.new
+    fti = Texticle::FullTextIndex.new('ft_index', fm) do
+      name
+    end
+    assert_equal "to_tsvector('english', name)", fti.to_s
+  end
+
+  def test_to_s_A_weight
+    fm = FakeModel.new
+    fti = Texticle::FullTextIndex.new('ft_index', fm) do
+      name 'A'
+    end
+    assert_equal "setweight(to_tsvector('english', name), 'A')", fti.to_s
+  end
+
+  def test_to_s_multi_weight
+    fm = FakeModel.new
+    fti = Texticle::FullTextIndex.new('ft_index', fm) do
+      name  'A'
+      value 'A'
+      description 'B'
+    end
+    assert_equal "setweight(to_tsvector('english', name, value), 'A') || ' ' || setweight(to_tsvector('english', description), 'B')", fti.to_s
+  end
+
+  def test_mixed_weight
+    fm = FakeModel.new
+    fti = Texticle::FullTextIndex.new('ft_index', fm) do
+      name
+      value 'A'
+    end
+    assert_equal "setweight(to_tsvector('english', value), 'A') || ' ' || to_tsvector('english', name)", fti.to_s
   end
 end
