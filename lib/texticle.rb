c627255e8501e023ffb7283edd3db9337bff4cd9
Matthias Viehweger <kronn@kronn.de>
2010-08-19 17:19:24 +0800

    prevented warning about defined constant

--- lib/texticle.rb
+++ lib/texticle.rb
@@ -49,7 +49,7 @@ require 'texticle/railtie' if defined?(Rails) and Rails::VERSION::MAJOR > 2
 #   end
 module Texticle
   # The version of Texticle you are using.
-  VERSION = '1.0.3'
+  VERSION = '1.0.4' unless defined?(Texticle::VERSION)
 
   # A list of full text indexes
   attr_accessor :full_text_indexes
