a41e0a7d07094736093e8dc106d3fe4150782ff9
Matthias Viehweger <kronn@kronn.de>
2010-08-19 19:06:45 +0800

    extract_method: constant lookup for models

--- lib/texticle/tasks.rb
+++ lib/texticle/tasks.rb
@@ -10,7 +10,7 @@ namespace :texticle do
       fh.puts "class FullTextSearch#{now.to_i} < ActiveRecord::Migration"
       fh.puts "  def self.up"
       Dir[Rails.root + 'app' + 'models' + '*.rb'].each do |f|
-        klass = File.basename(f, '.rb').pluralize.classify.constantize
+        klass = find_constant_of_model_in(f)
         if klass.respond_to?(:full_text_indexes)
           (klass.full_text_indexes || []).each do |fti|
             fh.puts <<-eostmt
@@ -32,7 +32,7 @@ namespace :texticle do
   desc "Create full text indexes"
   task :create_indexes => ['texticle:destroy_indexes'] do
     Dir[Rails.root + 'app' + 'models' + '*.rb'].each do |f|
-      klass = File.basename(f, '.rb').pluralize.classify.constantize
+      klass = find_constant_of_model_in(f)
       if klass.respond_to?(:full_text_indexes)
         (klass.full_text_indexes || []).each do |fti|
           begin
@@ -48,12 +48,16 @@ namespace :texticle do
   desc "Destroy full text indexes"
   task :destroy_indexes => [:environment] do
     Dir[Rails.root + 'app' + 'models' + '*.rb'].each do |f|
-      klass = File.basename(f, '.rb').pluralize.classify.constantize
+      klass = find_constant_of_model_in(f)
       if klass.respond_to?(:full_text_indexes)
         (klass.full_text_indexes || []).each do |fti|
           fti.destroy
         end
       end
     end
+  end
+
+  def find_constant_of_model_in(filename)
+    File.basename(filename, '.rb').pluralize.classify.constantize
   end
 end
