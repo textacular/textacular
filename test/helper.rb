9c4e0901108c22909ff2b70dced7cce08f68d0e9
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-14 09:45:27 -0800

    stop using File.expand_path()

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,3 @@
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
-
 require "test/unit"
 require "texticle"
 
--- test/test_texticle.rb
+++ test/test_texticle.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/helper')
+require 'helper'
 
 class TestTexticle < TexticleTestCase
   def test_index_method
