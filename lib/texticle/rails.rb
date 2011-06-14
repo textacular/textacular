c271b68ee8eba1902af491a3d6f26fbbfbdea7ff
ecin <ecin@copypastel.com>
2011-06-14 04:01:18 -0400

    Add instructions for non-Rails usage, rename railtie.rb to rails.rb

--- lib/texticle/rails.rb
+++ lib/texticle/rails.rb
@@ -1,12 +1,8 @@
 # Module used to conform to Rails 3 plugin API
-require File.expand_path( File.dirname(__FILE__) + '/../texticle')
+require File.expand_path(File.dirname(__FILE__) + '/../texticle')
 
 module Texticle
   class Railtie < Rails::Railtie
-    rake_tasks do
-      load File.dirname(__FILE__) + '/tasks.rb'
-    end
-
     initializer "texticle.configure_rails_initialization" do
       ActiveRecord::Base.extend(Texticle)
     end
