6eaf7581f2cf933320b57a31d0fce2baf9598cc1
Ben Hamill <git-commits@benhamill.com>
2011-10-04 22:24:29 -0500

    Expand abbreviation.

--- lib/texticle/tasks.rb
+++ lib/texticle/tasks.rb
@@ -2,7 +2,7 @@ require 'rake'
 require 'texticle'
 
 namespace :texticle do
-  desc 'Create FTS text index migration'
+  desc 'Create full text search index migration'
   task :create_index_migration => :environment do
     Texticle::FullTextIndexer.generate_migration
   end
