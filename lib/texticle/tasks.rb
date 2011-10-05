345ddc972632e622c038a626a75bae6024e028ed
Ben Hamill <git-commits@benhamill.com>
2011-10-04 23:07:52 -0500

    Object has some state now.

--- lib/texticle/tasks.rb
+++ lib/texticle/tasks.rb
@@ -4,6 +4,6 @@ require 'texticle'
 namespace :texticle do
   desc 'Create full text search index migration'
   task :create_index_migration => :environment do
-    Texticle::FullTextIndexer.generate_migration
+    Texticle::FullTextIndexer.new.generate_migration
   end
 end
