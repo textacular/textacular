73fe5dfa1ef1c68c0629953f01046fdfc2951533
Ben Hamill <git-commits@benhamill.com>
2012-01-29 14:20:47 -0600

    Factor out tons of search crap.

--- lib/texticle.rb
+++ lib/texticle.rb
@@ -2,22 +2,17 @@ require 'active_record'
 
 module Texticle
   def search(query = "", exclusive = true)
-    advanced_search(query, exclusive, false)
+    exclusive, query = munge_exclusive_and_query(exclusive, query)
+    parsed_query_hash = parse_query_hash(query)
+    similarities, conditions = basic_similarities_and_conditions(parsed_query_hash)
+    assemble_query(similarities, conditions, exclusive)
   end
 
-  def advanced_search(query = "", exclusive = true, advanced = true)
-    @similarities = []
-    @conditions = []
-
-    unless query.is_a?(Hash)
-      exclusive = false
-      query = searchable_columns.inject({}) do |terms, column|
-        terms.merge column => query.to_s
-      end
-    end
-
-    parse_query_hash(query, advanced)
-    assemble_query(@similarities, @conditions, exclusive)
+  def advanced_search(query = "", exclusive = true)
+    exclusive, query = munge_exclusive_and_query(exclusive, query)
+    parsed_query_hash = parse_query_hash(query)
+    similarities, conditions = advanced_similarities_and_conditions(parsed_query_hash)
+    assemble_query(similarities, conditions, exclusive)
   end
 
   def method_missing(method, *search_terms)
@@ -49,25 +44,62 @@ module Texticle
 
   private
 
-  def parse_query_hash(query, advanced, table_name = quoted_table_name)
-    language = connection.quote(searchable_language)
+  def munge_exclusive_and_query(exclusive, query)
+    unless query.is_a?(Hash)
+      exclusive = false
+      query = searchable_columns.inject({}) do |terms, column|
+        terms.merge column => query.to_s
+      end
+    end
+
+    [exclusive, query]
+  end
+
+  def parse_query_hash(query, table_name = quoted_table_name)
     table_name = connection.quote_table_name(table_name)
 
-    query.each do |column_or_table, search_term|
+    results = []
+
+    query.collect do |column_or_table, search_term|
       if search_term.is_a?(Hash)
-        parse_query_hash(search_term, advanced, column_or_table)
+        results += parse_query_hash(search_term, column_or_table)
       else
         column = connection.quote_column_name(column_or_table)
         search_term = connection.quote normalize(Helper.normalize(search_term))
-        if advanced
-          @similarities << "ts_rank(to_tsvector(#{language}, #{table_name}.#{column}::text), to_tsquery(#{language}, #{search_term}::text))"
-          @conditions   << "to_tsvector(#{language}, #{table_name}.#{column}::text) @@ to_tsquery(#{language}, #{search_term}::text)"
-        else
-          @similarities << "ts_rank(to_tsvector(#{language}, #{table_name}.#{column}::text), plainto_tsquery(#{language}, #{search_term}::text))"
-          @conditions   << "to_tsvector(#{language}, #{table_name}.#{column}::text) @@ plainto_tsquery(#{language}, #{search_term}::text)"
-        end
+
+        [table_name, column, search_term]
       end
     end
+  end
+
+  def basic_similarities_and_conditoins(parsed_query_hash)
+    parsed_query_hash.inject([]) do |memo, query_args|
+      memo << [basic_similarity_string(*query_args), basic_condition_string(*query_args)]
+      memo
+    end
+  end
+
+  def basic_similarity_string(table_name, column, query)
+    "ts_rank(to_tsvector(#{quoted_language}, #{table_name}.#{column}::text), plainto_tsquery(#{quoted_language}, #{search_term}::text))"
+  end
+
+  def basic_condition_string(table_name, column, query)
+    "to_tsvector(#{quoted_language}, #{table_name}.#{column}::text) @@ plainto_tsquery(#{quoted_language}, #{search_term}::text)"
+  end
+
+  def advanced_similarities_and_conditoins(parsed_query_hash)
+    parsed_query_hash.inject([]) do |memo, query_args|
+      memo << [advanced_similarity_string(*query_args), advanced_condition_string(*query_args)]
+      memo
+    end
+  end
+
+  def advanced_similarity_string(table_name, column, query)
+    "ts_rank(to_tsvector(#{quoted_language}, #{table_name}.#{column}::text), to_tsquery(#{quoted_language}, #{search_term}::text))"
+  end
+
+  def advanced_condition_string(table_name, column, query)
+    "to_tsvector(#{quoted_language}, #{table_name}.#{column}::text) @@ to_tsquery(#{quoted_language}, #{search_term}::text)"
   end
 
   def assemble_query(similarities, conditions, exclusive)
@@ -84,6 +116,10 @@ module Texticle
 
   def searchable_columns
     columns.select {|column| [:string, :text].include? column.type }.map(&:name)
+  end
+
+  def quoted_language
+    @quoted_language ||= connection.quote(searchable_language)
   end
 
   def searchable_language
