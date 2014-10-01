9230a93d737a0732ee38232e5ece4d39809f1042
Ben Hamill <git-commits@benhamill.com>
2014-10-01 16:25:08 -0500

    Establish DB connection.

--- spec/textacular_spec.rb
+++ spec/textacular_spec.rb
@@ -1,9 +1,15 @@
 require 'support/ar_stand_in'
+require 'support/not_there'
 
 RSpec.describe Textacular do
   context "after extending ActiveRecord::Base" do
     it "doesn't break #respond_to?" do
       expect{ ARStandIn.respond_to?(:abstract_class?) }.to_not raise_error
+    end
+
+    it "doesn't break #respond_to? for table-less classes" do
+      expect(NotThere.table_exists?).to be_falsey
+      expect { NotThere.respond_to? :system }.to_not raise_error
     end
   end
 end
