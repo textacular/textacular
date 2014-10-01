996e425e05030f9d35e52f5d66105ec411c68242
Ben Hamill <git-commits@benhamill.com>
2014-10-01 16:28:07 -0500

    Add method_missing sanity tests.

--- spec/textacular_spec.rb
+++ spec/textacular_spec.rb
@@ -11,5 +11,27 @@ RSpec.describe Textacular do
       expect(NotThere.table_exists?).to be_falsey
       expect { NotThere.respond_to? :system }.to_not raise_error
     end
+
+    it "doesn't break #method_missing" do
+      expect { ARStandIn.random }.to raise_error(NoMethodError)
+
+      begin
+        ARStandIn.random
+      rescue NoMethodError => error
+        expect(error.message).to match(/undefined method `random'/)
+      end
+    end
+
+    it "doesn't break #method_missing for table-less classes" do
+      expect(NotThere.table_exists?).to be_falsey
+
+      expect { NotThere.random }.to raise_error(NoMethodError)
+
+      begin
+        NotThere.random
+      rescue NoMethodError => error
+        expect(error.message).to match(/undefined method `random'/)
+      end
+    end
   end
 end
