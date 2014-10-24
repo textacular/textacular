de8aea67cbc92e10c53ec47baef37bfc0b55320d
Ben Hamill <git-commits@benhamill.com>
2014-10-23 23:31:18 -0500

    TrigramInstaller spec.

---
+++ spec/textacular/trigram_installer_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe "Textacular::TrigramInstaller" do
+  let(:content) do
+    <<-MIGRATION
+class InstallTrigram < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Base.connection.execute("CREATE EXTENSION pg_trgm;")
+  end
+
+  def self.down
+    ActiveRecord::Base.connection.execute("DROP EXTENSION pg_trgm;")
+  end
+end
+MIGRATION
+  end
+
+  it "generates a migration" do
+    generator = double(:migration_generator)
+
+    expect(Textacular::MigrationGenerator).to receive(:new).with('install_trigram', content).and_return(generator)
+    expect(generator).to receive(:generate_migration)
+
+    Textacular::TrigramInstaller.new.generate_migration
+  end
+end
