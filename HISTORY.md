ed651308365a4f5d86fc8a02ba1c423137c32fd8
ecin <ecin@copypastel.com>
2014-12-22 11:13:56 -0800

    Bump version to 3.2.1
    Add a HISTORY.md doc to track what's new.

---
+++ HISTORY.md
@@ -0,0 +1,3 @@
+# v3.2.1 12/22/14
+
+- we're ActiveRecord 4.2+ compatible until tests prove otherwise.
--- lib/textacular/version.rb
+++ lib/textacular/version.rb
@@ -1,5 +1,5 @@
 module Textacular
-  VERSION = '3.2.0'
+  VERSION = '3.2.1'
 
   def self.version
     VERSION
