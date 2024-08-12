0e854f8cceaefb5464ff3d2e3e17f206ebfc05a7
Inge Jørgensen <inge@elektronaut.no>
2024-08-12 14:06:55 +0200

    Add support for TestCase.fixture_paths

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,3 @@
-
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require 'textacular'
 
@@ -14,6 +13,10 @@ ActiveRecord::Base.establish_connection config.merge(:adapter => :postgresql)
 class BaseTest < ActiveSupport::TestCase
   include ActiveRecord::TestFixtures
 
-  self.fixture_path = "#{File.dirname(__FILE__)}/fixtures/"
+  if self.respond_to?(:fixture_paths=)
+    self.fixture_paths << "#{File.dirname(__FILE__)}/fixtures/"
+  else
+    self.fixture_path = "#{File.dirname(__FILE__)}/fixtures/"
+  end
   fixtures :all
 end
